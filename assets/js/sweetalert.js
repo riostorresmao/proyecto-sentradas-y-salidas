0fdd8510f9e25ac1a772c67a71c4f10612dcc8ab
riostorresmao <mauricio.rios0@misena.edu.co>
2022-03-20 09:53:20 -0500

    alerta

--- assets/js/sweetalert.js
+++ assets/js/sweetalert.js
@@ -1,5 +1,22 @@
 Swal.fire(
-    'Good job!',
-    'You clicked the button!',
+    'Hola,de nuevo!',
+    'Clic en elboton para iniciar!',
     'success'
   )
+
+  swal({
+    title: "Estas seguro?",
+    text: "Once deleted, you will not be able to recover this imaginary file!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("el cambio guardado", {
+        icon: "success",
+      });
+    } else {
+      swal("Your imaginary file is safe!");
+    }
+  });
